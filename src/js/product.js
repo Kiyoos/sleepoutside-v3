d5b7fae1d5b9f004097cdac84a417bff3c6796a8
Kiyoos <kiyoos@byui.edu>
2024-09-28 12:14:31 -0700

    NS - Cart accepts multiple items and shows in the cart tab.

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,23 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
+
+let products = [];
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  let currentCart = getLocalStorage("so-cart");
+  console.log(currentCart);
+  if (currentCart == null) {
+    products.push(product);
+    return addProductToCart(products);
+  }
+  currentCart.push(product);
+  addProductToCart(currentCart);
 }
 
 // add listener to Add to Cart button
