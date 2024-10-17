490c50e78cc4bcbb4c5d069220671c3076029f60
Kiyoos <kiyoos@byui.edu>
2024-10-16 18:20:55 -0700

    NS--Added a customizable alert to index.html

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,5 +1,9 @@
+import { publishAlert } from "./alert.mjs";
 import productList from "./productList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 productList(".product-list", "tents");
 loadHeaderFooter();
+
+// NS uncomment out the below if you would like to see the alerts
+// publishAlert();
