c6b7d63f818c492a2da8c4c6ec5806a7f237013c
bridgea13 <bri21025@byui.edu>
2024-10-23 22:51:10 -0400

    finished all required, attempted the stretch

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,9 +1,11 @@
 import { publishAlert } from "./alert.mjs";
 import productList from "./productList.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 
-productList(".product-list", "tents");
+
 loadHeaderFooter();
+const category = getParam("category");
+productList(".product-list", category);
 
 // NS uncomment out the below if you would like to see the alerts
 // publishAlert();
