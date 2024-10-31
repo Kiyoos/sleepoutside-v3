a8f42431897250191917077665674a954aeceb75
Kiyoos <kiyoos@byui.edu>
2024-10-30 18:04:03 -0700

    NS - Update function

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,5 @@
+import checkoutProcess from "./checkoutProcess.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+checkoutProcess.init("so-cart", "checkout-summary");
--- src/js/checkoutProcess.mjs
+++ src/js/checkoutProcess.mjs
@@ -1,35 +1,40 @@
 import { getLocalStorage } from "./utils.mjs";
 
-
-
 const checkoutProcess = {
-    key: "",
-    outputSelector: "",
-    list: [],
-    itemTotal: 0,
-    shipping: 0,
-    tax: 0,
-    orderTotal: 0,
-    init: function (key, outputSelector) {
-        this.key = key;
-        this.outputSelector = outputSelector;
-        this.list = getLocalStorage(key);
-        this.calculateItemSummary();
-    },
-    calculateItemSummary: function () {
+  key: "",
+  outputSelector: "",
+  list: [],
+  itemTotal: 0,
+  shipping: 0,
+  tax: 0,
+  orderTotal: 0,
+  init: function (key, outputSelector) {
+    this.key = key;
+    this.outputSelector = outputSelector;
+    this.list = getLocalStorage(key);
+    this.calculateItemSummary();
+  },
+  calculateItemSummary: function () {
+    // NS sums the FinalPrice from each item in the cart
+    const items = this.list;
+    const sumTotal = items.reduce((total, item) => {
+      return total + item.FinalPrice;
+    }, 0);
+    console.log(sumTotal);
+    const element = document.getElementById("itemTotal");
+    // NS formats the total to USD
+    const formatTotal = sumTotal.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    const itemQty = items.length;
+    return (element.innerHTML = `Item Subtotal(${itemQty}): ${formatTotal}`);
+  },
+  calculateOrderTotal: function () {
+    this.displayOrderTotals();
+  },
 
-    },
-    calculateOrderTotal: function () {
-
-
-        this.displayOrderTotals();
-
-    },
-
-    displayOrderTotals: function () {
-
-    }
-
-}
+  displayOrderTotals: function () {},
+};
 
 export default checkoutProcess;
