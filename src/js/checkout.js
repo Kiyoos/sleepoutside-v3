3e396122a558031c34ba6ae9cad3ed8434a1ebf0
LowanWill <lowannwil@byui.edu>
2024-10-30 20:01:41 -0600

    wednesday update

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -2,4 +2,25 @@ import checkoutProcess from "./checkoutProcess.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
-checkoutProcess.init("so-cart", "checkout-summary");
+checkoutProcess.init("so-cart", ".checkout-summary");
+
+document
+    .querySelector("#zip")
+    .addEventListener(
+        "blur",
+        checkoutProcess.calculateOrdertotal.bind(checkoutProcess)
+    );
+
+
+document.forms["checkout"].addEventListener("submit", (e) => {
+    e.preventDefault();
+    //e.target would contain our form in this case
+    checkoutProcess.checkout(e.target);
+});
+
+// listening for click on the button
+//document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+//  e.preventDefault();
+
+//checkoutProcess.checkout(document.forms['checkout']);
+//});
