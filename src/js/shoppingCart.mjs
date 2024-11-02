6961af5846bfb8b0e10d032587e2ff621991f1e7
Kiyoos <kiyoos@byui.edu>
2024-11-02 16:38:12 -0700

    NS Added dynamic quantities in the cart and updated the cart quantity icon

--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -37,10 +37,10 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1 
+  <p class="cart-card__quantity">qty: ${item.Quantity}
     <span class="remove" data-id="${item.Id}">&#10060;</span>
   </p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
+  <p class="cart-card__price">$${item.FinalPrice} each</p>
 </li>`;
 
   return newItem;
